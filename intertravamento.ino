187e3b6632a00de1fd34e60338a03d3875a6837c
antonio272a <antoniofelipe2001@hotmail.com>
2022-01-19 17:22:31 -0300

    V 1.0.0

--- intertravamento.ino
+++ intertravamento.ino
@@ -8,6 +8,16 @@
 #define RELAY_OUT_4 9
 #define INPUT_TYPE_SWITCH 10
 #define CONTROL_INPUT 11
+#define NUMBER_OF_INPUTS 4
+
+#define RELAY_ACTIVE_TIME 500
+#define ACTIVATED_SECURE_DELAY 5000
+
+int inputArray[NUMBER_OF_INPUTS][2] = {
+  {RELAY_IN_1, RELAY_OUT_1}, {RELAY_IN_2, RELAY_OUT_2}, {RELAY_IN_3, RELAY_OUT_3}, {RELAY_IN_4, RELAY_OUT_4}
+};
+
+bool activated = false;
 
 void setUpPins() {
   pinMode(RELAY_IN_1, INPUT_PULLUP);
@@ -34,14 +44,35 @@ void setup() {
   setRelaysHigh();
 }
 
-void checkInput() {
-  
+void triggerRelay(int output) {
+  digitalWrite(output, LOW);
+  delay(RELAY_ACTIVE_TIME);
+  digitalWrite(output, HIGH);
+}
+
+void checkInputs() {
+  for (int i = 0; i <= NUMBER_OF_INPUTS; i++) {
+    if (digitalRead(inputArray[i][0]) == LOW) {
+      triggerRelay(inputArray[i][1]);
+      activated = true;
+      break;
+    }
+  }
+}
+
+void checkActivated() {
+  if (activated) {
+    delay(ACTIVATED_SECURE_DELAY);
+    activated = false;
+  }
 }
 
 void loop() {
-  bool isOpen = digitalRead(CONTROL_INPUT) === LOW;
-  if(!isOpen) {
-    checkInputs();  
+  bool isOpen = digitalRead(CONTROL_INPUT) == LOW;
+  if (!isOpen) {
+    checkInputs();
   }
+
+  checkActivated();
 
 }
